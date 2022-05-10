6d4587d1c47730749bd2002fb3a16dea7b987a2c
ashutoshcgm <ashutoshexlcgm@gmail.com>
2022-05-10 20:20:10 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,67 @@
+
+let allMusic = [
+    {
+        name: "Backboner",
+        artist: "music-1",
+        img: "Music-1",
+        src: "Music-1"
+    },
+    {
+        name: "Bijlee-Bijlee",
+        artist: "music-2",
+        img: "Music-2",
+        src: "Music-2"
+    },
+    {
+        name: "ButterFly",
+        artist: "music-3",
+        img: "Music-3",
+        src: "Music-3"
+    },
+    {
+        name: "High Rated Gabru",
+        artist: "music-4",
+        img: "Music-4",
+        src: "Music-4"
+    },
+    {
+        name: "Laung lachi",
+        artist: "music-5",
+        img: "Music-5",
+        src: "Music-5"
+    },
+    {
+        name: "Patola",
+        artist: "music-6",
+        img: "Music-6",
+        src: "Music-6"
+    }
+    ,
+    {
+        name: "Sakhiyan 2.0",
+        artist: "music-7",
+        img: "Music-7",
+        src: "Music-7"
+    }
+    ,
+    {
+        name: "Same Beef",
+        artist: "music-8",
+        img: "Music-8",
+        src: "Music-8"
+    }
+    ,
+    {
+        name: "Suit Punjabi",
+        artist: "music-9",
+        img: "Music-9",
+        src: "Music-9"
+    }
+    ,
+    {
+        name: "Titliaan",
+        artist: "music-10",
+        img: "Music-10",
+        src: "Music-10"
+    }
+];
